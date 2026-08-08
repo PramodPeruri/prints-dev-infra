e93422d7a3a64fb30550795f4eed1dad53a208dd
pramod <peruripramoddevops@gmail.com>
2026-08-08 23:44:42 +0530

    infra

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -1,9 +1,18 @@
-# #frontend accepting traffic from frontend alb
-# resource "aws_security_group_rule" "frontend_frontend_alb" {
-#   type = ingress
-#   security_group_id = module.sg[9].sg_id # frontend SG ID
-#   source_security_group_id = module.sg[11].sg_id
-#   from_port         = 80
-#   protocol       = "tcp"
-#   to_port           = 80
-# }
+#frontend accepting traffic from frontend alb
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type = ingress
+  security_group_id = local.backend_alb_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 80
+  protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_security_group_rule" "bastion_laptop" {
+  type = ingress
+  security_group_id = local.backend_alb_sg_id
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 22
+  protocol       = "tcp"
+  to_port           = 22
+}
