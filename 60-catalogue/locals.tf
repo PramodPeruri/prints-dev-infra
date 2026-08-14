e0de4e6cbbd68b9f2cbb0a7ddaee732a0f4cf4b9
pramod <peruripramoddevops@gmail.com>
2026-08-14 14:59:54 +0530

    infra

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -4,6 +4,7 @@ locals {
    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
    vpc_id = data.aws_ssm_parameter.vpc_id
    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
    common_tags = {
        Project = var.project_name
        Environment = var.environment
