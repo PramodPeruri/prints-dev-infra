6998b683d851ea39c1ca4912c7df252610b92dd6
pramod <peruripramoddevops@gmail.com>
2026-08-14 15:08:52 +0530

    infra

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -168,3 +168,15 @@ resource "aws_lb_listener_rule" "catalogue" {
     }
   }
 }
+
+resource "terraform_data" "catalogue_local" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+  
+  depends_on = [ aws_lb_listener_rule.catalogue ]
+
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
+  }
+}
