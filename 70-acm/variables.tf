2b88fa34167fdd59ce431350f3c4a044fd44f766
pramod <peruripramoddevops@gmail.com>
2026-08-16 18:15:56 +0530

    infra

---
+++ 70-acm/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+    default = "prints"
+
+}
+
+variable "environment" {
+    default = "dev"
+
+}
+
+variable "zone_id" {
+    default = "Z05714843SO1YQWMF1N5N"
+
+}
+
+variable "domain_name" {
+    default = "pramod.shop"
+
+}
