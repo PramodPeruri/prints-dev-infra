67bfb10c25b439e0390f68d105af405993c0e901
pramod <peruripramoddevops@gmail.com>
2026-08-18 14:49:22 +0530

    infra

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -162,3 +162,58 @@ resource "aws_security_group_rule" "payment_backend_alb" {
   protocol       = "tcp"
   to_port           = 8080
 }
+
+
+resource "aws_security_group_rule" "catalogue_cart" {
+  type = "ingress"
+  security_group_id = local.catalogue_sg_id
+  source_security_group_id = local.cart_sg_id
+  from_port         = 8080
+  protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_security_group_rule" "cart_shipping" {
+  type = "ingress"
+  security_group_id = local.cart_sg_id
+  source_security_group_id = local.shipping_sg_id
+  from_port         = 8080
+  protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_security_group_rule" "user_payment" {
+  type = "ingress"
+  security_group_id = local.user_sg_id
+  source_security_group_id = local.payment_sg_id
+  from_port         = 8080
+  protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_security_group_rule" "cart_payment" {
+  type = "ingress"
+  security_group_id = local.cart_sg_id
+  source_security_group_id = local.payment_sg_id
+  from_port         = 8080
+  protocol       = "tcp"
+  to_port           = 8080
+}
+
+resource "aws_security_group_rule" "backend_alb_frontend" {
+  type = "ingress"
+  security_group_id = local.backend_alb_sg_id
+  source_security_group_id = local.frontend_sg_id
+  from_port         = 80
+  protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_security_group_rule" "frontend_frontend_alb" {
+  type = "ingress"
+  security_group_id = local.frontend_sg_id
+  source_security_group_id = local.frontend_alb_sg_id
+  from_port         = 80
+  protocol       = "tcp"
+  to_port           = 80
+}
