42af6177fbba68e6affa89599150a9f34a89ab87
pramod <peruripramoddevops@gmail.com>
2026-08-19 12:14:56 +0530

    infra

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -10,6 +10,7 @@ sudo yum -y install terraform
 
 cd /home/ec2-user
 git clone https://github.com/daws-86s/roboshop-dev-infra.git
+chown ec2-user:ec2-user -R roboshop-dev-infra
 cd prints-dev-infra/40-databases
 terraform init
 terraform apply -auto-approve
