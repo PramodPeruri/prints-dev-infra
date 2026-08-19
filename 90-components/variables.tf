6523db03bfb54d6cc6da6ac82ee20e1e2dbc716c
pramod <peruripramoddevops@gmail.com>
2026-08-19 14:10:43 +0530

    infra

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -29,3 +29,23 @@ variable "components" {
 
     }
 }
+
+variable "project_name" {
+    default = "prints"
+
+}
+
+variable "environment" {
+    default = "dev"
+
+}
+
+variable "zone_id" {
+    default = "Z05714843SO1YQWMF1N5N"
+
+}
+
+variable "domain_name" {
+    default = "pramod.shop"
+
+}
