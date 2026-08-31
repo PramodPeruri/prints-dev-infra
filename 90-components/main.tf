2c366db20a572bec4aaf9175f00bd534f207d3b5
pramod <peruripramoddevops@gmail.com>
2026-08-31 13:26:05 +0530

    infra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -7,6 +7,6 @@
 module "components" {
     for_each = var.components
     source= "git::https://github.com/PramodPeruri/terraform-prints-component.git?ref=main"
-    component = each.key
+    components = each.key
     rule_priority = each.value.rule_priority
 }
