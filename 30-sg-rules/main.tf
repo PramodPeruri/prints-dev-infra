64c3373a0f2219abf26af9a2e2185ee2a227d7fc
pramod <peruripramoddevops@gmail.com>
2026-08-31 13:00:38 +0530

    infra

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -272,14 +272,14 @@ resource "aws_security_group_rule" "mongodb_user" {
   to_port           = 27017
 }
 
-resource "aws_security_group_rule" "redis_user" {
-  type = "ingress"
-  security_group_id = local.redis_sg_id
-  source_security_group_id = local.user_sg_id
-  from_port         = 6379
-  protocol       = "tcp"
-  to_port           = 6379
-}
+# resource "aws_security_group_rule" "redis_user" {
+#   type = "ingress"
+#   security_group_id = local.redis_sg_id
+#   source_security_group_id = local.user_sg_id
+#   from_port         = 6379
+#   protocol       = "tcp"
+#   to_port           = 6379
+# }
 
 resource "aws_security_group_rule" "backend_alb_cart" {
   type = "ingress"
