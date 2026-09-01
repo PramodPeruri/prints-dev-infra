ea3a7c8690160d9dcb2e578dea710057085aa0db
pramod <peruripramoddevops@gmail.com>
2026-09-02 00:00:27 +0530

    infra

---
+++ 91-cloudfront/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    default = "prints"
+}
+
+variable "environment" {
+    default = "dev"
+
+
+}
+
+variable "zone_id" {
+    default = "Z05714843SO1YQWMF1N5N"
+
+}
+
+variable "domain_name" {
+    default = "pramod.shop"
+
+}
+
+variable "hosted_zone_id" {
+    default = "Z05714843SO1YQWMF1N5N"
+}
